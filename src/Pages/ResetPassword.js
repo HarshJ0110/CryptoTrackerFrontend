bbeb9f52e2e5aad671c2ebcadc3b5d83a3d44253
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 13:23:50 +0530

    Update ResetPassword.js

--- src/Pages/ResetPassword.js
+++ src/Pages/ResetPassword.js
@@ -17,7 +17,6 @@ const ResetPassword = () => {
     const resetPasswordSubmit = async (e) => {
         e.preventDefault();
         const response = await fetch(`${link}/api/user/password/reset`, {
-            mode: 'no-cors',
             method: "POST",
             headers: {
               "Content-Type": "application/json",
